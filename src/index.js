3c95115311aabe23e491b681918619f1bd50e909
Evgeniy <yaremenko.x@gmail.com>
2023-10-13 21:04:12 +0300

    update index.js

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ root.render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter basename="/HealthyHub-team_project">
+        <BrowserRouter basename="/healthyhub-frontend">
           <App />
         </BrowserRouter>
       </PersistGate>
