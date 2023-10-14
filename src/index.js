40471c8eae0a63d6f4af9ac43d78c61629f92b71
Evgeniy <yaremenko.x@gmail.com>
2023-10-14 22:07:36 +0300

    elimination of errors

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ root.render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter basename="/healthyhub-frontend">
+        <BrowserRouter basename="/healthyHub-frontend">
           <App />
         </BrowserRouter>
       </PersistGate>
